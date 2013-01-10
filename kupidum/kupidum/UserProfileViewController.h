41ae40d7d2fb6d75e930425dadb48b54427816e7
Albert Nadal Garriga <anadalg@gmail.com>
2013-01-10 17:48:46 +0100

    Added empty UserProfileViewController class

---
+++ kupidum/kupidum/UserProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  UserProfileViewController.h
+//  kupidum
+//
+//  Created by Albert Nadal Garriga on 10/01/13.
+//  Copyright (c) 2013 laFruitera.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UserProfileViewController : UIViewController
+
+@end
